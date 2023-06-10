6e2035e790f64ce5a9f37e9c27f6a1d665c5e20b
eliaspozuelos <eliaspozuelos256@gmail.com>
2023-06-10 07:47:14 -0600

    Interfaces XD

--- GestorContactoss/src/itp_library/Lendings.java
+++ GestorContactoss/src/itp_library/Lendings.java
@@ -6,6 +6,7 @@
 package itp_library;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,6 +19,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 /**
@@ -26,15 +28,18 @@ import javax.swing.JPanel;
  */
 public class Lendings extends javax.swing.JPanel {
 
-    Connect conn;
+ 
     Connection reg;
+    private Component rootPane;
     /**
      * Creates new form Principal
      */
     public Lendings() {
         initComponents();
-        conn = new Connect();
-        reg = conn.getConnection();
+        TextPrompt nombre = new TextPrompt("Ingrese su nombre", txt_nombre);
+        TextPrompt numero = new TextPrompt("Ingrese el numero telefonico", txt_numero);
+        TextPrompt correo = new TextPrompt("Ingrese el Correo electronico", txt_correo);
+       
     }
 
     /**
@@ -53,11 +58,18 @@ public class Lendings extends javax.swing.JPanel {
         jSeparator1 = new javax.swing.JSeparator();
         jSeparator2 = new javax.swing.JSeparator();
         jSeparator3 = new javax.swing.JSeparator();
-        book_id = new javax.swing.JTextField();
-        folio = new javax.swing.JTextField();
+        txt_numero = new javax.swing.JTextField();
+        txt_nombre = new javax.swing.JTextField();
         button = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         Image = new javax.swing.JLabel();
+        txt_correo = new javax.swing.JTextField();
+        jSeparator4 = new javax.swing.JSeparator();
+        Text3 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        Text4 = new javax.swing.JLabel();
+        btn_cancelar = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(255, 255, 255));
         setMinimumSize(new java.awt.Dimension(750, 430));
@@ -69,59 +81,62 @@ public class Lendings extends javax.swing.JPanel {
         add(body, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         Title.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
-        Title.setText("Nuevo Préstamo");
+        Title.setText("NUEVO CONTACTO ");
         add(Title, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, -1, -1));
 
         Text1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        Text1.setText("Libro ID");
-        add(Text1, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 150, -1, -1));
+        Text1.setText("Categoria ");
+        add(Text1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 320, -1, -1));
 
         Text2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        Text2.setText("Folio Usuario");
-        add(Text2, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 50, -1, -1));
+        Text2.setText("Nombre");
+        add(Text2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 80, -1, -1));
 
         jSeparator1.setForeground(new java.awt.Color(0, 153, 255));
         jSeparator1.setPreferredSize(new java.awt.Dimension(200, 10));
-        add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 210, 260, 10));
+        add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 290, 260, 10));
 
         jSeparator2.setForeground(new java.awt.Color(0, 153, 255));
         jSeparator2.setPreferredSize(new java.awt.Dimension(200, 10));
-        add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 110, 260, 10));
+        add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 130, 260, 10));
 
         jSeparator3.setForeground(new java.awt.Color(204, 204, 204));
         jSeparator3.setOrientation(javax.swing.SwingConstants.VERTICAL);
         jSeparator3.setPreferredSize(new java.awt.Dimension(200, 10));
-        add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 20, 10, 350));
+        add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 20, 10, 350));
 
-        book_id.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
-        book_id.setForeground(new java.awt.Color(102, 102, 102));
-        book_id.setText("Ingrese el ID del Libro a prestar");
-        book_id.setBorder(null);
-        book_id.addMouseListener(new java.awt.event.MouseAdapter() {
+        txt_numero.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        txt_numero.setForeground(new java.awt.Color(102, 102, 102));
+        txt_numero.setBorder(null);
+        txt_numero.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mousePressed(java.awt.event.MouseEvent evt) {
-                book_idMousePressed(evt);
+                txt_numeroMousePressed(evt);
             }
         });
-        book_id.addActionListener(new java.awt.event.ActionListener() {
+        txt_numero.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                book_idActionPerformed(evt);
+                txt_numeroActionPerformed(evt);
             }
         });
-        add(book_id, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 180, 260, 30));
+        txt_numero.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txt_numeroKeyTyped(evt);
+            }
+        });
+        add(txt_numero, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 180, 260, 30));
 
-        folio.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
-        folio.setForeground(new java.awt.Color(102, 102, 102));
-        folio.setText("Ingrese el folio del usuario");
-        folio.setBorder(null);
-        folio.addMouseListener(new java.awt.event.MouseAdapter() {
+        txt_nombre.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        txt_nombre.setForeground(new java.awt.Color(102, 102, 102));
+        txt_nombre.setBorder(null);
+        txt_nombre.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mousePressed(java.awt.event.MouseEvent evt) {
-                folioMousePressed(evt);
+                txt_nombreMousePressed(evt);
             }
         });
-        add(folio, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 80, 260, 30));
+        add(txt_nombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 100, 260, 30));
 
         button.setBackground(new java.awt.Color(18, 90, 173));
-        button.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        button.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         button.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 buttonMouseEntered(evt);
@@ -138,35 +153,93 @@ public class Lendings extends javax.swing.JPanel {
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Prestar");
+        jLabel1.setText("Agregar");
         button.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 10, -1, -1));
 
-        add(button, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 270, 260, 50));
+        add(button, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 270, 260, 50));
 
         Image.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        Image.setIcon(new javax.swing.ImageIcon(getClass().getResource("/itp_library/images/prestamo.gif"))); // NOI18N
+        Image.setIcon(new javax.swing.ImageIcon(getClass().getResource("/itp_library/images/agregarC.png"))); // NOI18N
+        Image.setAlignmentX(1055.0F);
+        Image.setAutoscrolls(true);
+        Image.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        Image.setDoubleBuffered(true);
+        Image.setFocusTraversalPolicyProvider(true);
         Image.setMaximumSize(new java.awt.Dimension(750, 430));
         Image.setMinimumSize(new java.awt.Dimension(750, 430));
-        add(Image, new org.netbeans.lib.awtextra.AbsoluteConstraints(-180, -140, -1, -1));
+        add(Image, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 10, 270, 240));
+        Image.getAccessibleContext().setAccessibleDescription("");
+
+        txt_correo.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        txt_correo.setForeground(new java.awt.Color(102, 102, 102));
+        txt_correo.setBorder(null);
+        txt_correo.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                txt_correoMousePressed(evt);
+            }
+        });
+        txt_correo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txt_correoActionPerformed(evt);
+            }
+        });
+        txt_correo.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txt_correoKeyTyped(evt);
+            }
+        });
+        add(txt_correo, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 260, 260, 30));
+
+        jSeparator4.setForeground(new java.awt.Color(0, 153, 255));
+        jSeparator4.setPreferredSize(new java.awt.Dimension(200, 10));
+        add(jSeparator4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 210, 260, 10));
+
+        Text3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        Text3.setText("Numero");
+        add(Text3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 160, -1, -1));
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Famila", "amigos", "trabajo" }));
+        add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 350, 180, 30));
+
+        Text4.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        Text4.setText("Correo electronico ");
+        add(Text4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 240, -1, -1));
+
+        btn_cancelar.setBackground(new java.awt.Color(18, 90, 173));
+        btn_cancelar.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btn_cancelar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btn_cancelarMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btn_cancelarMouseExited(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                btn_cancelarMousePressed(evt);
+            }
+        });
+        btn_cancelar.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Cancelar");
+        btn_cancelar.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 10, -1, -1));
+
+        add(btn_cancelar, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 340, 260, 50));
     }// </editor-fold>//GEN-END:initComponents
 
-    private void book_idActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_book_idActionPerformed
+    private void txt_numeroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_numeroActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_book_idActionPerformed
+    }//GEN-LAST:event_txt_numeroActionPerformed
 
-    private void folioMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_folioMousePressed
-       if(folio.getText().equals("Ingrese el folio del usuario"))
-        folio.setText("");
-       if(book_id.getText().equals("") || book_id.getText() == null || book_id.getText().equals(" "))
-        book_id.setText("Ingrese el ID del Libro a prestar");
-    }//GEN-LAST:event_folioMousePressed
+    private void txt_nombreMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txt_nombreMousePressed
+   
+    }//GEN-LAST:event_txt_nombreMousePressed
 
-    private void book_idMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_book_idMousePressed
-        if(book_id.getText().equals("Ingrese el ID del Libro a prestar"))
-            book_id.setText("");
-        if(folio.getText().equals("") || folio.getText() == null || folio.getText().equals(" "))
-            folio.setText("Ingrese el folio del usuario");
-    }//GEN-LAST:event_book_idMousePressed
+    private void txt_numeroMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txt_numeroMousePressed
+    
+    }//GEN-LAST:event_txt_numeroMousePressed
 
     private void buttonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_buttonMouseEntered
         setColor(button);
@@ -177,60 +250,49 @@ public class Lendings extends javax.swing.JPanel {
     }//GEN-LAST:event_buttonMouseExited
     // PRESTAR
     private void buttonMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_buttonMousePressed
-        String fol = folio.getText();
-        String book = book_id.getText();
-        int intfol = 0;
-        
+     
         // Conditions
-        if(fol.equals("") || book.equals("")){
-            javax.swing.JOptionPane.showMessageDialog(this, "Debe llenar todos los campos \n", "AVISO", javax.swing.JOptionPane.INFORMATION_MESSAGE);
-            folio.requestFocus();
-        }
-        else{
-            try
-            {
-                intfol = Integer.parseInt(folio.getText());
-                
-                if(intfol <= 0){
-                    javax.swing.JOptionPane.showMessageDialog(this, "El folio del usuario debe ser mayor a 0. \n", "AVISO", javax.swing.JOptionPane.INFORMATION_MESSAGE);
-                     folio.requestFocus();
-                }
-            }catch(Exception ex){
-                javax.swing.JOptionPane.showMessageDialog(this, "El folio del usuario debe ser un número entero. \n", "AVISO", javax.swing.JOptionPane.INFORMATION_MESSAGE);
-                 folio.requestFocus();
-            }            
-        }
         
-        try {
-            // Verificamos el usuario
-            boolean pase = UserExist(intfol);
-            if(!pase){
-                javax.swing.JOptionPane.showMessageDialog(this, "No existe ningún usuario con ese Folio. \n", "AVISO", javax.swing.JOptionPane.INFORMATION_MESSAGE);
-                 folio.requestFocus();
-            }
-            // Verificamos el libro
-            else if(!BookExist(book)){
-                javax.swing.JOptionPane.showMessageDialog(this, "No existe ningún libro con esa ID. \n", "AVISO", javax.swing.JOptionPane.INFORMATION_MESSAGE);
-                 book_id.requestFocus();
-            }
-            else if(CheckLending(intfol, book)){
-                javax.swing.JOptionPane.showMessageDialog(this, "Esa persona ya cuenta con el préstamo de ese mismo Libro. \n", "AVISO", javax.swing.JOptionPane.INFORMATION_MESSAGE);
-                 book_id.requestFocus();
-            }
-            else if(!BookAvailable(book)){
-                javax.swing.JOptionPane.showMessageDialog(this, "Ya no hay más libros con esa ID para prestar. \n", "AVISO", javax.swing.JOptionPane.INFORMATION_MESSAGE);
-                 book_id.requestFocus();
-            }
-            else{
-                InsertLending(intfol, book);// Insertamos el prestamo a la DB.
-                folio.setText("");
-                book_id.setText("");
-            }
-              
-        } catch (SQLException ex) {
-            Logger.getLogger(Lendings.class.getName()).log(Level.SEVERE, null, ex);
-        }
     }//GEN-LAST:event_buttonMousePressed
+
+    private void txt_numeroKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txt_numeroKeyTyped
+        // TODO add your handling code here:
+        char validar = evt.getKeyChar();
+        if(Character.isLetter(validar)){
+            getToolkit().beep();
+            evt.consume();
+            JOptionPane.showMessageDialog(rootPane,"Debe Ingresar solo numeros");
+        }
+    }//GEN-LAST:event_txt_numeroKeyTyped
+
+    private void txt_correoMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txt_correoMousePressed
+       
+    }//GEN-LAST:event_txt_correoMousePressed
+
+    
+    
+    
+    
+    
+    private void txt_correoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_correoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txt_correoActionPerformed
+
+    private void txt_correoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txt_correoKeyTyped
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txt_correoKeyTyped
+
+    private void btn_cancelarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_cancelarMouseEntered
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btn_cancelarMouseEntered
+
+    private void btn_cancelarMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_cancelarMouseExited
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btn_cancelarMouseExited
+
+    private void btn_cancelarMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_cancelarMousePressed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btn_cancelarMousePressed
 
     void setColor(JPanel panel){
         panel.setBackground(new Color(21,101,192));
@@ -403,14 +465,21 @@ public class Lendings extends javax.swing.JPanel {
     private javax.swing.JLabel Image;
     private javax.swing.JLabel Text1;
     private javax.swing.JLabel Text2;
+    private javax.swing.JLabel Text3;
+    private javax.swing.JLabel Text4;
     private javax.swing.JLabel Title;
     private javax.swing.JPanel body;
-    private javax.swing.JTextField book_id;
+    private javax.swing.JPanel btn_cancelar;
     private javax.swing.JPanel button;
-    private javax.swing.JTextField folio;
+    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator2;
     private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JSeparator jSeparator4;
+    private javax.swing.JTextField txt_correo;
+    private javax.swing.JTextField txt_nombre;
+    private javax.swing.JTextField txt_numero;
     // End of variables declaration//GEN-END:variables
 }
